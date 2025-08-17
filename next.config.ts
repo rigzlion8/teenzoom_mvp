7ec4e50991b9fb8e6c18a1bc47e461af453275a1
Reagan Musa <reagan.musav@gmail.com>
2025-08-18 00:11:36 +0300

    fix auth vercel build issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@prisma/client'],
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), '@prisma/client']
+    return config
+  }
 };
 
 export default nextConfig;
